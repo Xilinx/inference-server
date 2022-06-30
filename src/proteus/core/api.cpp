77e0361b831ef18e169c606284b72579c80114a0
Varun Sharma <varun.sharma@amd.com>
2022-06-30 13:09:34 -0700

    Refactor modelLoad and modelUnload

---
+++ src/proteus/core/api.cpp
@@ -0,0 +1,53 @@
+// Copyright 2022 Xilinx Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ * @brief Implements the client-server API used in the inference server
+ */
+
+#include "proteus/core/api.hpp"
+
+#include <algorithm>
+#include <string>
+
+#include "proteus/core/manager.hpp"
+#include "proteus/core/model_repository.hpp"
+#include "proteus/core/predict_api.hpp"
+#include "proteus/core/worker_info.hpp"
+
+namespace proteus {
+
+void modelLoad(const std::string& model, RequestParameters* parameters) {
+  for (const auto& c : model) {
+    assert(c == std::tolower(c));
+  }
+  if (parameters == nullptr) {
+    RequestParameters params;
+    ModelRepository::modelLoad(model, &params);
+    Manager::getInstance().loadWorker(model, params);
+  } else {
+    ModelRepository::modelLoad(model, parameters);
+    Manager::getInstance().loadWorker(model, *parameters);
+  }
+}
+
+void modelUnload(const std::string& model) {
+  for (const auto& c : model) {
+    assert(c == std::tolower(c));
+  }
+  Manager::getInstance().unloadWorker(model);
+}
+
+}  // namespace proteus
---
+++ src/proteus/core/api.hpp
@@ -0,0 +1,34 @@
+// Copyright 2022 Xilinx Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ * @brief Defines the client-server API used in the inference server
+ */
+
+#ifndef GUARD_PROTEUS_CORE_API
+#define GUARD_PROTEUS_CORE_API
+
+#include <string>
+
+namespace proteus {
+
+class RequestParameters;
+
+void modelLoad(const std::string& model, RequestParameters* parameters);
+void modelUnload(const std::string& model);
+
+}  // namespace proteus
+
+#endif  // GUARD_PROTEUS_CORE_API
