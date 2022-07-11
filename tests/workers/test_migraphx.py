cbe2bcb3b53fd718dd3280038a8a1886cea56727
Varun Sharma <varun.sharma@amd.com>
2022-07-11 09:37:20 -0600

    Remove hardcoded GID in Dockerfile
    Add flag to build image with  MIGraphX
    Update MIGraphX test golden values
    Undo changes

--- tests/workers/test_migraphx.py
+++ tests/workers/test_migraphx.py
@@ -23,17 +23,17 @@ import cv2
 from helper import run_benchmark, root_path
 
 sys.path.insert(0, os.path.join(root_path, "examples/python"))
-from utils.utils import preprocess, postprocess
+from utils.utils import postprocess
 
 
 # The make_nxn and preprocess functions are based on an migraphx example at
 # AMDMIGraphx/examples/vision/python_resnet50/resnet50_inference.ipynb
 # The mean and standard dev. values used for this normalization are requirements of the Resnet50 model.
 
-""" crop an image to square and then resize to desired dimension n x n"""
-
-
 def make_nxn(image, n):
+    """ 
+    Crop an image to square and then resize to desired dimension n x n
+    """
     width = image.shape[1]
     height = image.shape[0]
     if height > width:
@@ -49,12 +49,16 @@ def make_nxn(image, n):
     else:
         return cv2.resize(image, (n, n))
 
-
-"""# Normalize array values to the data type, mean and std. dev. required by Resnet50
-# img_data: numpy array in 3 dimensions [channels, rows, cols] with value range 0-255"""
-
-
 def preprocess(img_data):
+    """
+    Normalize array values to the data type, mean and std. dev. required by Resnet50
+
+    Args:
+        img_data (np.array): array in 3 dimensions [channels, rows, cols] with value range 0-255
+
+    Returns:
+        Response: np.array
+    """
     mean_vec = np.array([0.485, 0.456, 0.406])
     stddev_vec = np.array([0.229, 0.224, 0.225])
     norm_img_data = np.zeros(img_data.shape).astype("float32")
@@ -72,7 +76,7 @@ def model_fixture():
 def parameters_fixture():
     return {
         "model": str(
-            root_path / "external/artifacts/migraphx/resnet50-v1-7/resnet50-v1-7.onnx"
+            root_path / "external/artifacts/migraphx/resnet50v2/resnet50-v2-7.onnx"
         )
     }
 
@@ -118,15 +122,14 @@ class TestMigraphx:
                 assert output.parameters.empty()
         return response
 
-    def test_Migraphx_0(self):
+    @pytest.mark.parametrize("num", [1, 8])
+    def test_migraphx(self, num):
         """
         Send a request to model as tensor data
         """
         image_path = str(root_path / "tests/assets/dog-3619020_640.jpg")
 
-        preprocessing = {"input_size": 224, "model": self.model}
-
-        batch = 1
+        batch = num
         images = []
         for _ in range(batch):
             # Load a picture
@@ -141,32 +144,7 @@ class TestMigraphx:
         response = self.send_request(request)
         k = postprocess(response, 5)
         # Look for the top 5 categories (not underlying scores)
-        gold_response_output = [259, 261, 283, 230, 157]
-        assert (k == gold_response_output).all()
-
-    def test_Migraphx_1(self):
-        """
-        Send a request to model as tensor data
-        """
-        image_path = str(root_path / "tests/assets/dog-3619020_640.jpg")
-
-        preprocessing = {"input_size": 224, "resize_method": "crop"}
-
-        batch = 8
-        images = []
-        for _ in range(batch):
-            # Load a picture
-            img = cv2.imread(image_path).astype("float32")
-            # Crop to a square, resize
-            img = make_nxn(img, 224)
-            #  Normalize contents with values specific to Resnet50
-            img = img.transpose(2, 0, 1)
-            img = preprocess(img)
-            images.append(img)
-        request = proteus.ImageInferenceRequest(images, True)
-        response = self.send_request(request)
-        k = postprocess(response, 5)
-        gold_response_output = [259, 261, 283, 230, 157]
+        gold_response_output = [259, 261, 157, 154, 230]
         assert (k == gold_response_output).all()
 
     @pytest.mark.benchmark(group="Migraphx")
