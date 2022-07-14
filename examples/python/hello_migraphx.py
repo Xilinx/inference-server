6e241db072bf308f5c4999343f943879b8a8f790
Brian Pickrell <bpickrel@amd.com>
2022-07-14 12:25:16 -0700

    update image normalization vectors for migraphx worker tests.  Since inputs to normalization were changed from BGR color channel format to RGB, vectors are reordered to match.  Also updates test gold values.

--- examples/python/hello_migraphx.py
+++ examples/python/hello_migraphx.py
@@ -71,10 +71,11 @@ def preprocess(img_data):
 
     Args:
         img_data (np.array): 3 dimensions [channels, rows, cols] with value range 0-255
+        the vectors are for RGB images, so images read with OpenCV must have channels 
+        converted before calling.
     """
-    # todo: are these vectors based on RGB images or BGR?  Results seem good
-    mean_vec = np.array([0.485, 0.456, 0.406])
-    stddev_vec = np.array([0.229, 0.224, 0.225])
+    mean_vec = np.array([0.406, 0.456, 0.485])
+    stddev_vec = np.array([0.225, 0.224, 0.229])
     norm_img_data = np.zeros(img_data.shape).astype("float32")
     for i in range(img_data.shape[0]):
         norm_img_data[i, :, :] = (img_data[i, :, :] / 255 - mean_vec[i]) / stddev_vec[i]
@@ -156,7 +157,7 @@ def parse_args():
         required=False,
         default=os.path.join(
             root, "tests/assets/dog-3619020_640.jpg"
-        ),  # a girl in a sweatshirt
+        ),  # a dog
         help="An image to try inference on.  Use git-lfs to pull image assets",
     )
 
