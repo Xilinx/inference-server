d6620b0d5d06e6a8b37303f4aac4d82c0a38aca2
rradjabi <rradjabi@gmail.com>
2023-03-30 16:07:14 -0600

    move quickstart-setup.sh

--- examples/resnet50/quickstart-setup.sh
+++ examples/resnet50/quickstart-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#server setup for CPU
-
+## Quick Start setup for Server Deployment
 wget -O tensorflow.zip https://www.xilinx.com/bin/public/openDownload?filename=tf_resnetv1_50_imagenet_224_224_6.97G_2.5.zip
 unzip -j "tensorflow.zip" "tf_resnetv1_50_imagenet_224_224_6.97G_2.5/float/resnet_v1_50_baseline_6.96B_922.pb" -d .
 mkdir -p model_repository/resnet50/1
@@ -22,3 +21,10 @@ outputs [
         shape: [1000]
     }
 ]' > model_repository/resnet50/config.pbtxt
+
+
+## Quick Start setup for Client Inference
+wget https://github.com/Xilinx/inference-server/raw/main/examples/resnet50/imagenet_classes.txt
+wget https://github.com/Xilinx/inference-server/raw/main/tests/assets/dog-3619020_640.jpg
+wget https://github.com/Xilinx/inference-server/raw/main/examples/resnet50/resnet.py
+wget https://github.com/Xilinx/inference-server/raw/main/examples/resnet50/tfzendnn.py
